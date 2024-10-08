d2607c91d4ebf8686802b73fc2ec9a04a54c2599
Shadow <dark35854@gmail.com>
2024-10-08 17:22:55 +0530

    Desktop_mobile_navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,116 @@
+"use client"
+import { usePathname } from "next/navigation"
+import { Logo } from "./logo"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "./ui/button"
+import { Avatar } from "./Avatar"
+import { ThemeSwitcherBtn } from "./ThemeSwitcherBtn"
+import { useState } from "react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import { Menu } from "lucide-react"
+import { signOut } from "next-auth/react"
+
+export default function(){
+    return <>
+       <DesktopNavbar/>
+       <MobileNavbar/>
+    </>
+}
+
+const items = [
+    {label :"Dashboard" , link:"/"},
+    {label :"Transactions" , link:"/transactions"},
+    {label :"Manage" , link:"/manage"},
+]
+
+function MobileNavbar(){
+    const [isOpen , setIsOpen] = useState(false)
+    
+    return <div className="block border-separate bg-background md:hidden">
+        <nav className=" container flex items-center justify-center px-8">
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+                <SheetTrigger asChild >
+                    <Button variant={"ghost"} size={"icon"}>
+                        <Menu/>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent className="w-[400px] sm:w-[540px]">
+                    <div className=" flex flex-col gap-1 pt-4">
+                        {items.map(item=> <NavbarItem 
+                            key={item.label}
+                            link={item.link}
+                            label={item.label}
+                        />)}
+                    </div>
+                </SheetContent>
+            </Sheet>
+            <div className=" flex h-[80px] min-h-[60px] items-center gap-x-4">
+                <Logo />
+              
+            </div>
+            <div className="flex items-center gap-2">
+               <ThemeSwitcherBtn />
+               {/* <Button onClick={()=>signOut()} variant={"secondary"} >
+                Signout
+               </Button> */}
+               <Avatar/>
+            </div>
+        </nav>
+    </div>
+
+}
+
+function DesktopNavbar() {
+    return (
+        <div className="hiddden border-separate border-b bg-background md:block ">
+            <nav className=" container flex items-center justify-between px-8">
+                <div className=" flex h-[80px] min-h-[60px] items-center gap-x-4">
+                <Logo/>
+                <div className=" flex h-full">
+                   {items.map((item)=> (
+                    <NavbarItem
+                     key={item.label}
+                     link={item.link}
+                     label={item.label}
+                    />
+                   ))}
+                </div>
+
+                </div>
+                   <div className=" flex items-center gap-2">
+                        <ThemeSwitcherBtn />
+                        <Avatar/>
+                   </div>
+            </nav>
+
+        </div>
+    )
+}
+
+const NavbarItem=({link , label}:{
+    link: string;
+    label: string;
+})=>{
+    const pathname = usePathname();
+    const isActive = pathname === link;
+
+    return <div className=" relative flex items-center">
+        <Link href={link} className={cn(
+         buttonVariants({
+            variant:"ghost"
+         }),
+         "w-full justify-center text-lg text-muted-foreground hover:text-foreground"
+        )}>
+          {label}
+        </Link>
+        {
+            isActive && (
+                <div className=" absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block">
+
+                </div>
+            )
+        }
+    </div>
+
+}
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,13 @@
-import {Cat} from "lucide-react"
+import {Snail} from "lucide-react"
 import Link from "next/link"
 export const Logo=()=>{
     return <Link href={"/"} className="flex items-center gap-2">
-     <Cat className=" str" />
+     <Snail className="stroke h-10 w-10 stroke-emerald-400 stroke-[1.5]" />
+     <p className=" bg-gradient-to-r from-emerald-500 to-emerald-900 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">MoneyMap</p>
+    </Link>
+}
+export const LogoMobile=()=>{
+    return <Link href={"/"} className="flex items-center gap-2">
+     <p className=" bg-gradient-to-r from-emerald-500 to-emerald-900 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">MoneyMap</p>
     </Link>
 }
