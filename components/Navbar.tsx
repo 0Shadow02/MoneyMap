375627f0594375d82e4f45a7127ee305a3051fe9
Shadow <dark35854@gmail.com>
2024-10-08 19:48:19 +0530

    responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,16 +28,18 @@ function MobileNavbar(){
     const [isOpen , setIsOpen] = useState(false)
     
     return <div className="block border-separate bg-background md:hidden">
-        <nav className=" container flex items-center justify-center px-8">
+        <nav className=" container flex items-center justify-between px-8">
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
                 <SheetTrigger asChild >
                     <Button variant={"ghost"} size={"icon"}>
                         <Menu/>
                     </Button>
                 </SheetTrigger>
-                <SheetContent className="w-[400px] sm:w-[540px]">
+                <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+                    <Logo/>
                     <div className=" flex flex-col gap-1 pt-4">
-                        {items.map(item=> <NavbarItem 
+                        {items.map(item=> <NavbarItem
+                            clickCallback={()=> setIsOpen((prev)=>!prev)}
                             key={item.label}
                             link={item.link}
                             label={item.label}
@@ -63,7 +65,7 @@ function MobileNavbar(){
 
 function DesktopNavbar() {
     return (
-        <div className="hiddden border-separate border-b bg-background md:block ">
+        <div className="hidden border-separate border-b bg-background md:block ">
             <nav className=" container flex items-center justify-between px-8">
                 <div className=" flex h-[80px] min-h-[60px] items-center gap-x-4">
                 <Logo/>
@@ -88,9 +90,10 @@ function DesktopNavbar() {
     )
 }
 
-const NavbarItem=({link , label}:{
+const NavbarItem=({link , label , clickCallback}:{
     link: string;
     label: string;
+    clickCallback? : () => void
 })=>{
     const pathname = usePathname();
     const isActive = pathname === link;
@@ -100,8 +103,13 @@ const NavbarItem=({link , label}:{
          buttonVariants({
             variant:"ghost"
          }),
-         "w-full justify-center text-lg text-muted-foreground hover:text-foreground"
-        )}>
+         "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
+         isActive && "text-foreground"
+        )}
+        onClick={()=>{
+            if(clickCallback) clickCallback()
+        }}
+        >
           {label}
         </Link>
         {
