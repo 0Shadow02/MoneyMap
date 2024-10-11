2aa04b2b6721caa16596bf37c80b38ec958b6d16
Shadow <dark35854@gmail.com>
2024-10-11 18:06:23 +0530

    added_react-query

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,16 +1,25 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from "react";
 export default function RootProviders({children}:{children:React.ReactNode}){
-    return<ThemeProvider 
+    const [queryClient] = React.useState(()=>  new QueryClient({}))
+  
+    return (
+        <QueryClientProvider client={queryClient}>
+
+    <ThemeProvider 
              attribute="class"
              defaultTheme="dark"
              enableSystem 
              disableTransitionOnChange
-    
-    >
+             
+             >
         {children}
  
     </ThemeProvider>
-}
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+)}
