adc6635f5f41acafce53ca4f380a3adb49885f91
Shadow <dark35854@gmail.com>
2024-10-17 08:22:24 +0530

    added_graphs

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -1,12 +1,23 @@
-" use client"
+"use client" 
 
 import { Badge } from "@/components/ui/badge";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { GetFormatterForCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
 import { useMemo, useState } from "react";
 import HistoryPeriodSelector from "./HistoryPeriodSelector";
+import { useQuery } from "@tanstack/react-query";
+import Skeletonwrap from "@/components/Skeleton";
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
 
 export default function History({userSettings}:{userSettings:UserSettings}) {
     const [timeframe, setTimeframe] = useState<Timeframe>("month")
@@ -19,6 +30,17 @@ export default function History({userSettings}:{userSettings:UserSettings}) {
         return GetFormatterForCurrency(userSettings.currency);
 
     },[userSettings.currency])
+
+    const historyDataQuery = useQuery({
+        queryKey: ["overview", "history" , timeframe, period],
+        queryFn: async () => {
+            const response = await fetch(`/api/history-data?timeframe=${timeframe}&year=${period.year}&month=${period.month}`)
+
+            return response.json()
+        }
+    })
+
+    const dataAvailable = historyDataQuery.data && historyDataQuery.data.length > 0
 
     return <div  className=" container">
             <h2 className="mt-12 text-3xl font-bold">
@@ -43,6 +65,65 @@ export default function History({userSettings}:{userSettings:UserSettings}) {
                         </div>
                     </CardTitle>
                 </CardHeader>
+                <CardContent>
+                    <Skeletonwrap isLoading={historyDataQuery.isFetching}>
+                            {dataAvailable && <ResponsiveContainer width={"100%"} height={300}>
+                                <BarChart 
+                                height={300} 
+                                data={historyDataQuery.data}
+                                barCategoryGap={5}
+                                >
+                                    <defs>
+                                        <linearGradient
+                                         id="incomeBar"
+                                         x1="0"
+                                         y1="0"
+                                         x2="0"
+                                         y2="1"
+                                        >
+                                            <stop offset={"0"} stopColor="#10b981" stopOpacity={"1"}/>
+                                            
+                                            <stop offset={"1"} stopColor="#10b981" stopOpacity={"0"}/>
+                                           
+                                        </linearGradient>
+                                        <linearGradient
+                                         id="expenseBar"
+                                         x1="0"
+                                         y1="0"
+                                         x2="0"
+                                         y2="1"
+                                        >
+                                            <stop offset={"0"} stopColor="#ef4444" stopOpacity={"1"}/>
+
+                                            <stop offset={"1"} stopColor="#ef4444" stopOpacity={"0"}/>
+                                           
+                                        </linearGradient>
+                                    </defs>
+                                    <CartesianGrid strokeDasharray="5 5" strokeOpacity={"0.2"} vertical={false} />
+                                    <XAxis
+                                      stroke="#888888"
+                                      fontSize={12}
+                                      tickLine={false}
+                                      axisLine={false}
+                                      padding={{left: 5 , right: 5}}
+                                      dataKey={(data)=>{
+                                        const {year , month, day} = data;
+                                        const date = new Date(year, month, day || 1);
+                                        
+                                      }} 
+                                    />
+                                </BarChart>
+                                </ResponsiveContainer>}
+                            {!dataAvailable && (
+                                <Card className="flex h-[300px] flex-col items-center justify-center bg-background">
+                                    No data for the selected period
+                                    <p className=" text-sm text-muted-foreground">
+                                        Try selecting a different period or adding new transactions
+                                    </p>
+                                </Card>
+                            )}
+                    </Skeletonwrap>
+                </CardContent>
             </Card>
     </div>
 }
--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import { GetHistoryPeriodsResponseType } from "@/app/api/history-periods/route";
+import Skeletonwrap from "@/components/Skeleton";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Period, Timeframe } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
 
 interface Props {
     period: Period;
@@ -10,5 +15,89 @@ interface Props {
 }
 
 export default function HistoryPeriodSelector({ period, setPeriod, timeframe, setTimeframe }:Props) {
+    const historyPeriods = useQuery<GetHistoryPeriodsResponseType>({
+        queryKey: ["overview", "history" ,"periods"],
+        queryFn: async () => {
+            const response = await fetch("/api/history-periods")
+            return response.json()
+        }
+    })
+    return (
+        <div className=" flex flex-wrap items-center gap-4">
+            <Skeletonwrap isLoading={historyPeriods.isFetching} fullwidth={false} >
+                <Tabs value={timeframe}
+                      onValueChange={(value) => setTimeframe(value as Timeframe)}
+                >
+                    <TabsList>
+                        <TabsTrigger value="year">Year</TabsTrigger>
+                        <TabsTrigger value="month">Month</TabsTrigger>
+                    </TabsList>
+                </Tabs>
+            </Skeletonwrap>
+            <div className="flex flex-wrap items-center gap-2">
+                <Skeletonwrap isLoading={historyPeriods.isFetching}  >
+                   <YearSelector period={period} setPeriod={setPeriod} years={historyPeriods.data || []} />
+                </Skeletonwrap>
+                {timeframe === "month" && (
+                    <Skeletonwrap isLoading={historyPeriods.isFetching} fullwidth={false}>
+                        <MonthSelector period={period} setPeriod={setPeriod} />
+                    </Skeletonwrap>
+                )}
+            </div>
 
+        </div>
+    )
+
+}
+
+function YearSelector({ period, setPeriod, years }: { period: Period; setPeriod: (period: Period) => void; years: GetHistoryPeriodsResponseType }) {
+    return (
+        <Select
+            value={period.year.toString()}
+           onValueChange={(value) =>{
+            setPeriod({
+                month: period.month,
+                year: parseInt(value),
+            })
+           }} >
+            <SelectTrigger className=" w-[180px]">
+                <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+                {years.map((year) => (
+                    <SelectItem key={year} value={year.toString()}>
+                        {year}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    )
+}
+
+
+function MonthSelector({ period, setPeriod }: { period: Period; setPeriod: (period: Period) => void; }) {
+    return (
+        <Select
+            value={period.month.toString()}
+           onValueChange={(value) =>{
+            setPeriod({
+                year: period.year,
+                month: parseInt(value),
+            })
+           }} >
+            <SelectTrigger className=" w-[180px]">
+                <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+                {[0,1,2,3,4,5,6,7,8,9,10,11].map((month) => {
+                    const monthStr = new Date(period.year,month ,1).toLocaleString("default", { month: "long" })
+
+                    return (
+                    <SelectItem key={month} value={month.toString()}>
+                        {monthStr}
+                    </SelectItem>
+                )})}
+            </SelectContent>
+        </Select>
+    )
 }
