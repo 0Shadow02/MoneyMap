f86996b61ba5a52bce9e84fb7384cf5d1b54fe8e
Shadow <dark35854@gmail.com>
2024-10-28 22:27:02 +0530

    navbar_allignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ function MobileNavbar(){
     const [isOpen , setIsOpen] = useState(false)
     
     return <div className="block border-separate bg-background md:hidden">
-        <nav className=" container flex items-center justify-between px-8">
+        <nav className=" container flex items-center justify-between px-2">
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
                 <SheetTrigger asChild >
                     <Button variant={"ghost"} size={"icon"}>
