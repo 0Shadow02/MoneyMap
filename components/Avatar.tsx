26658af62723cd1ceb6c29d760c21045fd9790f4
Shadow <dark35854@gmail.com>
2024-10-28 22:48:56 +0530

    avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,8 +1,15 @@
+"use client"
+import { useSession } from "next-auth/react"
+
 
 export const Avatar=()=>{
+    const session = useSession()
+    const profiePic = session?.data?.user?.image
     return <div>  
 <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-    <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+  {profiePic ? <img src={profiePic} alt="profile picture" className="w-full h-full object-cover" /> :  <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>} 
+   
+   
 </div>
 
     </div>
