9309675a0b23d1f83a88d612c5c88f5932993bdc
Shadow <dark35854@gmail.com>
2024-11-01 13:47:01 +0530

    loader

--- app/(dashboard)/loading.tsx
+++ app/(dashboard)/loading.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import animeConfusedGif from "./anime-confused.gif";
 
 const Loading: React.FC = () => {
     return (
-        <div style={{ textAlign: 'center', padding: '20px' }}>
-            <p className='text-3xl'>Loading...</p>
+        <div className=' flex justify-center items-center mt-20' style={{ textAlign: 'center', padding: '20px' }}>
+            <div>
+                <img src={animeConfusedGif.src} alt="Loading" />
+            </div>
         </div>
     );
 };
